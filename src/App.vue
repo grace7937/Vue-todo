87416d1fd495532b7a87fd9ea16f581dceb8c778
grace7937 <eunchong.k153@gmail.com>
2021-08-04 20:12:39 +0900

    completed

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="container">
     <h1 class="text-center">Todo App</h1>
     <input 
+      v-model="todoInputText"
       type="text" 
       class="w-100 p-2" 
       placeholder="type todo"
@@ -9,22 +10,23 @@
       >
       
   <hr>
-  <Todo :todos="todos" v-for="(todo, index) in todos" :key="index" :todo="todo"/>
+  <Todo :todos="todos" v-for="(todo, index) in todos" :key="index" :todo="todo" @toggle-checkbox="toggleCheckbox" @remove-todo="removeTodo"/>
     
   </div>
 </template>
 
 <script>
 
-import Todo from "../src/components/Toto.vue";
+import Todo from "./components/Todo.vue";
 export default {
   components: {
     Todo
   },
   data() {
     return {
+      todoInputText:'',
       todos: [
-        { id: 1 , text: 'buy a 1', checked: false },
+      
       ]
     }
   },
@@ -32,15 +34,24 @@ export default {
   methods: {
     addTodo(e) {
       const todoText = e.target.value;
-      const todo = { id: Math.random , text: todoText, checked: false }
-      this.todos.push(todo);
+      const todo = { id: this.todos.length === 0 ? 1 : this.todos[this.todos.length - 1].id + 1 , text: todoText, checked: false }
+      this.todos = [...this.todos, todo];                 // ...문법을 이용해 add
 
-      e.target.value = "";
+      this.todoInputText = "";
     },
-    removeTodo(e) {
-      todos.map((todo) => {
-        
-      })
+    removeTodo(id) {
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos.splice(index, 1);                       //splice를 이용해 remove
+    },
+    toggleCheckbox({id, checked}) {
+      console.log(id,checked);
+     const index = this.todos.findIndex(todo => {
+       return todo.id === id;
+     });
+     this.todos[index].checked = checked;
+
     }
   }
   
